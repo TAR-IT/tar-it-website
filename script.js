41fbc8d0e4660d148c68520b00d7123f1c01b20f
Tobias A. Rau <github@tar-it.com>
2024-02-12 22:54:07 +0100

    Add navbar toggle

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// Define toggleNavbar function
 function toggleNavbar() {
-    var navbarItems = document.getElementById("navbarItems");
-    navbarItems.classList.toggle("show");
-  }
-  
+  var navbarItems = document.getElementById("navbarItems");
+  navbarItems.classList.toggle("show");
+}
+
+// Attach event listeners to navbar links
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all navbar links
+  var navbarLinks = document.querySelectorAll('.navbar-items li a');
+
+  // Attach event listener to each link
+  navbarLinks.forEach(link => {
+      link.addEventListener('click', () => {
+          // Close the navbar
+          toggleNavbar();
+      });
+  });
+});
